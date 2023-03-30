6fa030f232ce43a3bbf354195d13315ad8070a9c
Shaikh Mueez <71594607+Moiz439@users.noreply.github.com>
2023-03-30 14:16:53 +0530

    Update RectangleEx.java

--- 30-03-2023/RectangleEx.java
+++ 30-03-2023/RectangleEx.java
@@ -1,27 +1,27 @@
-class Rect
+class Rect  // Create Rect class .
 {
-	int length;
-	int bredth;
-	Rect()
+	int length; //Declare length variable
+	int bredth;  //Declare bredth variable
+	Rect()   // create a no-argument constructor .
 	{
-		length=20;
-		bredth=30;
+		length=20; // initialize the variable
+		bredth=30;// initialize the variable
 	}
-	public void area()
+	public void area()  // create area method().
 	{
 		System.out.println("Area of Ractangle :"+(length*bredth));
 	}
-	public void perimeter()
+	public void perimeter() // create perimeter() method.
 	{
 		System.out.println("Perimeter of Rectangle :"+(2*length*bredth));
 	}
 }
-public class RectangleEx {
+public class RectangleEx {  // create RectangleEx class.
 
-	public static void main(String[] args) {
-		Rect r=new Rect();
-		r.area();
-        r.perimeter();
+	public static void main(String[] args) {  // create main() method.
+		Rect r=new Rect();  //create a object of Rect class.
+		r.area(); // call area() method.
+                r.perimeter(); // call perimeter() method.
 	}
 
 }
