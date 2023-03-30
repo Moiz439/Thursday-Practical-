8b5352c7f0f6fa3b6ffe4d54dea3ff284503e589
Shaikh Mueez <71594607+Moiz439@users.noreply.github.com>
2023-03-30 14:07:37 +0530

    Update OverloadingEx.java

--- 30-03-2023/OverloadingEx.java
+++ 30-03-2023/OverloadingEx.java
@@ -1,20 +1,19 @@
-class Over
+class Over   // Create Over class here.
 {
-	public void add(int a,int b)
+	public void add(int a,int b) // initialize the add() method with 2 parameters.
 	{
 		System.out.println("2 Arguments method is called...!");
 	}
-	public void add(int a,int b,int c)
+	public void add(int a,int b,int c) // initialize the add() method with 3 parameters.
 	{
 		System.out.println("3 Arguments method is called...!");
 	}
 }
-public class OverloadingEx {
+public class OverloadingEx { // create OverloadingEx class here.
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        Over o1=new Over();
-        o1.add(10,2,30);
+	public static void main(String[] args) {  // create main() method.
+        Over o1=new Over();  // Create object of Over class.
+        o1.add(10,2,30);  // call add() method.
 	}
 
 }
