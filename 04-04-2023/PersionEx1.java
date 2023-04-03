2b72089f55d4463e6f47f19aea31a97a033d8a59
Shaikh Mueez <71594607+Moiz439@users.noreply.github.com>
2023-04-03 13:22:59 +0530

    Update PersionEx1.java

--- 04-04-2023/PersionEx1.java
+++ 04-04-2023/PersionEx1.java
@@ -1,18 +1,18 @@
-class Person 
+class Person // create Person class.
 {
 	String name;
 	int age;
-	void speak(String name,int age)
+	void speak(String name,int age) //create speak() method.
 	{
 		this.name=name;
 		this.age=age;
 		System.out.println("Name :"+name+"\t Age :"+age);
 	}
 }
-class Student extends Person
+class Student extends Person // create Student class and extends Person class.
 {
 	String gread;
-	void study(String name,int age,String gread)
+	void study(String name,int age,String gread) // create stydy() method.
 	{
         this.name=name;
         this.age=age;
@@ -20,12 +20,12 @@ class Student extends Person
 		System.out.println("\nName :"+name+"\t Age :"+age+"\t Gread :"+gread);
 	}
 }
-public class PersionEx1 {
+public class PersionEx1 { // create main class .
 
-	public static void main(String[] args) {
-		Student s=new Student();
-		s.speak("ABC", 30);
-		s.study("Mueez", 20, "A+");
+	public static void main(String[] args) {  // create a main() method.
+		Student s=new Student(); // create an object of Student class.
+		s.speak("ABC", 30); // call spleak() method.
+		s.study("Mueez", 20, "A+"); // call study() method.
 	}
 
 }
